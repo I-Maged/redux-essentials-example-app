6321881cd1f8dd86615b2128b2120bda455635a5
I-Maged <magedhanafy@gmail.com>
2022-12-24 23:40:22 +0200

    notifications slice

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,15 +1,27 @@
 import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { fetchNotifications } from '../features/notifications/notificationsSlice'
 
 export const Navbar = () => {
+  const dispatch = useDispatch()
+
+  const fetchNewNotifications = () => {
+    dispatch(fetchNotifications())
+  }
+
   return (
     <nav>
       <section>
         <h1>Redux Essentials Example</h1>
-
         <div className="navContent">
           <div className="navLinks">
             <Link to="/">Homepage</Link>
+            <Link to="/users">Users</Link>
+            <Link to="/notifications">Notifications</Link>
           </div>
+          <button className="button" onClick={fetchNewNotifications}>
+            Refresh Notifications
+          </button>
         </div>
       </section>
     </nav>
